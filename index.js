e53ed14bbc58f72b4136eec036f017090229c34a
MR-Addict <2750417853@qq.com>
2022-10-03 22:16:31 +0800

    add local and online modes

--- index.js
+++ index.js
@@ -19,28 +19,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/", async (req, res) => {
   try {
-    if (!req.files) {
-      console.log("No file uploaded");
-      res.send({ status: false, message: "No file uploaded" });
-    } else {
-      if (req.files.captcha) {
-        const captcha = req.files.captcha;
-        captcha.mv("uploads/captcha.jpg");
-        PythonShell.run("captcha.py", null, function (err, results) {
-          if (err) {
-            console.log(err);
-            res.send({ status: false, message: "Solving failed" });
-          } else {
-            const response = { status: true, message: results[0] };
-            console.log(response);
-            res.send(response);
-          }
-        });
-      } else {
-        const response = { status: false, message: "Upload file key should be captcha" };
+    if (req.body.type == "local") {
+      if (!req.files) {
+        const response = { status: false, message: "No file uploaded" };
         console.log(response);
         res.send(response);
+      } else {
+        if (req.files.captcha) {
+          const captcha = req.files.captcha;
+          captcha.mv("uploads/captcha.jpg");
+          PythonShell.run(
+            "captcha.py",
+            { args: ["-t", "local", "-d", "uploads/captcha.jpg"] },
+            function (err, results) {
+              if (err) {
+                const response = { status: false, message: "Solving failed" };
+                console.log(response);
+                res.send(response);
+              } else {
+                const response = { status: true, message: results[0] };
+                console.log(response);
+                res.send(response);
+              }
+            }
+          );
+        } else {
+          const response = { status: false, message: "Upload file key should be captcha" };
+          console.log(response);
+          res.send(response);
+        }
       }
+    } else if (req.body.type == "online") {
+      const captcha_url = req.body.captcha;
+      PythonShell.run("captcha.py", { args: ["-t", "online", "-d", captcha_url] }, function (err, results) {
+        if (err) {
+          const response = { status: false, message: "Solving failed" };
+          console.log(response);
+          res.send(response);
+        } else {
+          const response = { status: true, message: results[0] };
+          console.log(response);
+          res.send(response);
+        }
+      });
+    } else {
+      const response = { status: false, message: "Type error" };
+      console.log(response);
+      res.send(response);
     }
   } catch (err) {
     res.status(500).send(err);
@@ -49,4 +74,4 @@ app.post("/", async (req, res) => {
 
 //start app
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Captcha APP listening on http://localhost:${port}`));
+app.listen(port, () => console.log(`Captcha app listening on http://localhost:${port}`));
--- public/index.js
+++ public/index.js
@@ -1,17 +1,68 @@
-function loadImage(event) {
+function loadLocalImage(event) {
+  console.log("img uploaded!");
   if (!event.target.files[0]) return;
-  const image = document.querySelector("img");
+  const image = document.getElementById("local_img");
   image.src = URL.createObjectURL(event.target.files[0]);
-  uploadImage();
+  uploadLocalImage();
 }
 
-function uploadImage() {
+function uploadLocalImage() {
   const formData = new FormData();
-  const captcha = document.querySelector("input").files[0];
+  const captcha = document.getElementById("captcha_local").files[0];
   if (!captcha) return;
 
+  formData.append("type", "local");
   formData.append("captcha", captcha);
   fetch("/", { method: "POST", body: formData })
     .then((response) => response.json())
-    .then((response) => (document.querySelector(".result p").innerHTML = response.message));
+    .then((response) => (document.querySelector("#local_form .result p").innerText = response.message));
 }
+
+function loadOnlineImage(event) {
+  img_url = event.target.value;
+  const image = document.getElementById("online_img");
+  image.src = img_url;
+  uploadOnlineImage(img_url);
+}
+
+function uploadOnlineImage(img_url) {
+  const formData = new FormData();
+
+  formData.append("type", "online");
+  formData.append("captcha", img_url);
+  fetch("/", { method: "POST", body: formData })
+    .then((response) => response.json())
+    .then((response) => (document.querySelector("#online_form .result p").innerText = response.message));
+}
+
+// set image error image
+function img_error(event) {
+  event.target.src = "404.png";
+}
+
+function showTab(num) {
+  local_button = document.getElementById("local_button");
+  online_button = document.getElementById("online_button");
+  local_form = document.getElementById("local_form");
+  online_form = document.getElementById("online_form");
+  if (num) {
+    local_button.style.background = "var(--pri-grey)";
+    local_button.style.color = "var(--pri-dark-grey)";
+    online_button.style.background = "var(--pri-green)";
+    online_button.style.color = "var(--pri-white)";
+    local_form.style.display = "none";
+    online_form.style.display = "";
+  } else {
+    local_button.style.background = "var(--pri-green)";
+    local_button.style.color = "var(--pri-white)";
+    online_button.style.background = "var(--pri-grey)";
+    online_button.style.color = "var(--pri-dark-grey)";
+    local_form.style.display = "";
+    online_form.style.display = "none";
+  }
+}
+
+showTab(0);
+
+local_button.addEventListener("click", () => showTab(0));
+online_button.addEventListener("click", (event) => showTab(1));
