821d058f801aa78ea1b69d4846d1dcc0f5821e20
MR-Addict <2750417853@qq.com>
2022-10-04 20:17:09 +0800

    hide progress bar

--- test/download.ps1
+++ test/download.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = 'silentlycontinue'
+$ProgressPreference = 'SilentlyContinue'
 
-for ($i = 0; $i -lt 100; $i++) {
+for ($i = 0; $i -lt 10; $i++) {
     $file_path = "../dataset/tmp/" + ([int64](([datetime]::UtcNow) - (get-date "1/1/1970")).TotalMilliseconds) + ".jpg"
     Invoke-WebRequest -Uri "https://u.njtech.edu.cn/cas/captcha.jpg" -OutFile $file_path
     $captcha = python captcha.py -t local -d $file_path
